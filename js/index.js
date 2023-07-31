b2c5afbdc4bca23f328b65fe8b9a31f7cae7bca5
avlir14299 <yuri14299@gmail.com>
2023-08-01 04:26:06 +0900

    history.html

--- js/index.js
+++ js/index.js
@@ -143,7 +143,6 @@ window.addEventListener('resize', () => {
 });
 
 window.addEventListener('scroll', () => {
-  console.log(scrollY);
 
   if (scrollY > 400) {
     header.classList.remove('off');
@@ -164,9 +163,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 window.addEventListener('scroll', () => {
-  console.log(scrollY);
   const storyText = document.querySelectorAll('.story__texterea');
   const storyTitle = document.querySelector('.story__title');
+  const storyImg = document.querySelectorAll('.story__imgs');
   let i = 660;
   let j = 1000;
 
@@ -175,7 +174,22 @@ window.addEventListener('scroll', () => {
   } else if (scrollY < i || scrollY > 5000) {
     storyTitle.classList.remove('on');
   };
+
   storyText.forEach(ele => {
+
+    if (scrollY > i && scrollY < j) {
+      ele.classList.add('on');
+    } else if (scrollY < i || scrollY > j) {
+      ele.classList.remove('on');
+    };
+    i += 500;
+    j += 500;
+  });
+
+  i = 660;
+  j = 1000;
+
+  storyImg.forEach(ele => {
     if (scrollY > i && scrollY < j) {
       ele.classList.add('on');
     } else if (scrollY < i || scrollY > j) {
@@ -184,6 +198,7 @@ window.addEventListener('scroll', () => {
 
     i += 500;
     j += 500;
-  })
+  });
 
-})
+
+});
